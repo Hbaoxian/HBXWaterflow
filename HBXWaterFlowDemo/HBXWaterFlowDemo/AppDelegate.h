03d83b2d963af44c1ebac846cb59ce99d4746b14
黄保贤 <huangbaoxian@edaijia-inc.cn>
2016-06-13 15:41:36 +0800

    创建工程

---
+++ HBXWaterFlowDemo/HBXWaterFlowDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBXWaterFlowDemo
+//
+//  Created by 黄保贤 on 16/6/13.
+//  Copyright © 2016年 黄保贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HBXWaterFlowDemo/HBXWaterFlowDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HBXWaterFlowDemo
+//
+//  Created by 黄保贤 on 16/6/13.
+//  Copyright © 2016年 黄保贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
